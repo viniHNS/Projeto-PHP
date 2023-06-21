ad1c0244fc81829c42b7c91f7993193972e74c40
Vinicius Hissayoshi Nishida <88986579+viniHNS@users.noreply.github.com>
2023-06-21 12:47:42 +0000

    adicionado consultar feriado

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php include('php/header.php'); ?>
 <?php include('php/navbar.php'); ?>
 
-
 <?php 
     $url_img = 'https://shibe.online/api/cats?count=1&urls=true&httpsUrls=false';
     @$dados_img = json_decode(file_get_contents($url_img));
@@ -16,14 +15,15 @@
             break;
         case 'ajuda':
             include('php/help.php');
-            break;       
+            break;   
+        case 'feriado':
+            include('php/viewFeriado.php');
+            break;        
         default:
             echo "<div class='d-flex justify-content-center'><h1 class='display-4 mt-3'>Bem vindo ao site!</h1></div>";
             echo "<div class='d-flex justify-content-center'><figure class='figure mt-1 mb-5 justify-content-center'><img src='$dados_img[0]' alt='Foto de um gatinho aleatário' class='rounded mx-auto d-block'><figcaption class='figure-caption text-center'>Foto de um gatinho aleatório.</figcaption></figure></div>";
     }
-
 ?>
-
 
 <?php include('php/footer.php'); ?>
 
---
+++ php/viewFeriado.php
@@ -0,0 +1,5 @@
+<?php 
+    $form_value = $_POST["feriado_input"];
+
+    
+?>
