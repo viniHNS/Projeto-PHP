c65276d35648a5ae777186d7de5a13b536a7ea53
Vinicius Hissayoshi Nishida <88986579+viniHNS@users.noreply.github.com>
2023-06-21 12:38:50 +0000

    alterado estilo

--- php/help.php
+++ php/help.php
@@ -1,4 +1,4 @@
 
-<div class="container mt-5 mx-auto justify-content-center">
+<div class="container mt-5 mx-auto d-flex justify-content-center">
     <p>Algumas funcionalidades ainda não estão disponíveis. E alguns que estão disponíveis, dependem de serviços de terceiros</p>
 </div>
