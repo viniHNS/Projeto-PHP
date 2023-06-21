35a0034d87023071ad2be2c4d6a2219d779c2533
Vinicius Hissayoshi Nishida <88986579+viniHNS@users.noreply.github.com>
2023-06-21 14:29:07 +0000

    melhorada formatacao

--- php/help.php
+++ php/help.php
@@ -1,4 +1,3 @@
-
 <div class="container mt-5 mx-auto d-flex justify-content-center">
     <p>Algumas funcionalidades ainda não estão disponíveis. E alguns que estão disponíveis, dependem de serviços 
     de terceiros, como o <a href="https://brasilapi.com.br/" target="_blank">Brasil API</a></p>
